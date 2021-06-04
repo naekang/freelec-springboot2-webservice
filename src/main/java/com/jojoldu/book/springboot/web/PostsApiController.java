72998632ee682395754f765802c8913aa7480382
naekang <66901361+naekang@users.noreply.github.com>
2021-06-04 14:56:50 +0900

    add registration, modification, delete function & google ouath 2.0 setting

--- src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -18,6 +18,7 @@ public class PostsApiController {
         return postsService.save(requestDto);
     }
 
+    // 게시글 수정 API
     @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
@@ -26,5 +27,11 @@ public class PostsApiController {
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
     }
 }
