ef2555c9d7803788742f301b392e229d3beaf03a
naekang <66901361+naekang@users.noreply.github.com>
2021-06-07 13:31:51 +0900

    add naver login function & modify test code

--- src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -6,7 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class HelloResponseDtoTest {
 
     @Test
-    public void lombokTest() {
+    public void 롬복_기능_테스트() {
         // given
         String name = "test";
         int amount = 1000;
